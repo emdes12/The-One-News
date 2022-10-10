f4f781155b7091e69e81e1e41819d834cf197d93
Alawiye Muritala <99465163+emdes12@users.noreply.github.com>
2022-10-10 23:28:39 +0100

    improving the change origin

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//getting news 
+//getting news
 
-const newsList = document.querySelector('.news-container');
-const otherNews = document.querySelector('.recent-news');
-const worldNews = document.querySelector('.world-news')
+const newsList = document.querySelector(".news-container");
+const otherNews = document.querySelector(".recent-news");
+const worldNews = document.querySelector(".world-news");
 
 //async function getNews() {
 //    const resp = await fetch('https://newsapi.org/v2/top-headlines?country=ng&apiKey=1d199936d6f74336847f277033333a1d');
@@ -15,10 +15,6 @@ const worldNews = document.querySelector('.world-news')
 
 //console.log(getNews())
 
-
-
-
-
 //function addNews(newsData) {
 
 //    console.log(incomeNews)
@@ -28,17 +24,19 @@ const worldNews = document.querySelector('.world-news')
 
 //https://newsapi.org/v2/top-headlines?country=ng&apiKey=1d199936d6f74336847f277033333a1d
 
-
-function sourceNews (country) {
-fetch('https://api.currentsapi.services/v1/latest-news?country='+ country +'&apiKey=MiAFjxEMKgrSf-9BMrBQ6Y35GAYLwqqMr7nZUHr-WodSUSx0')
-    .then(async response => {
-        let res = await response.json();
-        return res;
+function sourceNews(country) {
+  fetch(
+    "https://api.currentsapi.services/v1/latest-news?country=" +
+      country +
+      "&apiKey=MiAFjxEMKgrSf-9BMrBQ6Y35GAYLwqqMr7nZUHr-WodSUSx0"
+  )
+    .then(async (response) => {
+      let res = await response.json();
+      return res;
     })
-    .then(response => {
-
-        for (let i = 0; i < 10; i++) {
-            newsList.innerHTML += `<li class="news-list">
+    .then((response) => {
+      for (let i = 0; i < 10; i++) {
+        newsList.innerHTML += `<li class="news-list">
                 <a href="${response.news[i].url}" target="_blank"><img loading="lazy" src="${response.news[i].image}" alt="${response.news[i].title}">
 
                 <div class="news-details">
@@ -50,20 +48,22 @@ fetch('https://api.currentsapi.services/v1/latest-news?country='+ country +'&api
                     <div class="source">Source: <span>${response.news[i].author}</span></div>
                 </div></a>
             </li>
-        `
+            `;
+        console.log("worked");
+      }
+    });
 
-        }
-        
+  fetch(
+    "https://api.currentsapi.services/v1/latest-news?country=" +
+      country +
+      "&apiKey=MiAFjxEMKgrSf-9BMrBQ6Y35GAYLwqqMr7nZUHr-WodSUSx0"
+  )
+    .then((response) => {
+      return response.json();
     })
-
-fetch('https://api.currentsapi.services/v1/latest-news?country='+ country +'&apiKey=MiAFjxEMKgrSf-9BMrBQ6Y35GAYLwqqMr7nZUHr-WodSUSx0')
-    .then(response => {
-        return response.json();
-    })
-    .then(response => {
-
-        for (let i = 10; i < 23; i++) {
-            otherNews.innerHTML += `<li class="recent-list">
+    .then((response) => {
+      for (let i = 10; i < 23; i++) {
+        otherNews.innerHTML += `<li class="recent-list">
                 <a href="${response.news[i].url}" target="_blank"><img src="${response.news[i].image}" alt="${response.news[i].title}">
 
                 <div class="news-recent">
@@ -72,72 +72,72 @@ fetch('https://api.currentsapi.services/v1/latest-news?country='+ country +'&api
                     <div class="source">Source: <span>${response.news[i].author}</span></div>
                 </div></a>
             </li>
-        `
-
-        }
-    })
+        `;
+      }
+    });
 }
 
+sourceNews("ng");
 
 //end of getting news
 
 //changing the region(country)
+const selectEl = document.getElementById("country-select");
 
+selectEl.addEventListener("click", function () {
+  let newContri = this.value;
+  sourceNews(newContri);
 
-function changeRegion(region = "ng") {
-    let contri = region;
-    let fnChNews = sourceNews(contri);
+  //   let contri = region;
+  //   let fnChNews = sourceNews(contri);
 
-    setInterval(fnChNews, 3000);
-    console.log(region)
-}
-changeRegion()
+  //   setInterval(fnChNews, 3000);
+  //   console.log(region);
+});
 
 //changing region ends
 
 //slider images
 
-const images = document.querySelector('.images').children
-const dots = document.querySelector('.slider-bar').children
-const prevBtn = document.querySelector('.prev')
-const nextBtn = document.querySelector('.next')
+const images = document.querySelector(".images").children;
+const dots = document.querySelector(".slider-bar").children;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
-
-console.log(images)
+console.log(images);
 
 let change = 3;
 
-function slideImage (move) {
+function slideImage(move) {
+  images[change].classList.remove("real");
+  dots[change].classList.remove("img-on");
 
-    images[change].classList.remove('real')
-    dots[change].classList.remove('img-on')
+  switch (move) {
+    case "next":
+      change++;
+      break;
+    case "prev":
+      change--;
+      break;
+  }
 
-    switch(move) {
-        case 'next': 
-            change++;
-            break;
-        case 'prev':
-            change--;
-            break;
-    }
+  if (change >= images.length) {
+    change = 0;
+  }
 
-    if (change >= images.length) {
-        change = 0;
-    }
+  if (change < 0) {
+    change = 4;
+  }
 
-    if (change < 0) {
-        change = 4;
-    }
+  images[change].classList.add("real");
+  dots[change].classList.add("img-on");
 
-    images[change].classList.add('real')
-    dots[change].classList.add('img-on')
-
-    if (change === 4) {
-        prevBtn.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-        nextBtn.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-    } else {
-        prevBtn.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-        nextBtn.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-    }
+  if (change === 4) {
+    prevBtn.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    nextBtn.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+  } else {
+    prevBtn.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    nextBtn.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+  }
 }
 //changeRegion()
